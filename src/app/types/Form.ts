0321a3aa326b8341398bf9816b3c6ddc6d678c7b
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-05-19 08:07:00 +0700

    Membuat type untuk biodata form create dan edit

--- src/app/types/Form.ts
+++ src/app/types/Form.ts
@@ -7,3 +7,12 @@ interface UserFormProps {
   };
   onSubmit: (form: { name: string; email: string; role: string }) => void;
 }
+
+// form create/edit biodata
+interface BiodataForm {
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  photo: File | null;
+}
